d7ce9ae53167eccd18aa7751afb05e63e9515a98
trale <trale@DESKTOP-O6O2RKO>
2023-02-07 15:44:30 +0200

    Add project files.

---
+++ Tableproj/Model/ExcelClass.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Excel;
+
+namespace Tableproj.Model
+{
+    internal class ExcelClass
+    {
+        public Application ex { get; set; }
+
+        public ExcelClass()
+        {
+            ex = new Application();
+            ex.Workbooks.Add(Type.Missing);
+            ex.Workbooks[1].Activate();
+            ex.Sheets.Add(Type.Missing);
+            ((Worksheet)this.ex.ActiveWorkbook.Sheets[1]).Select();
+        }
+
+        public ExcelClass(string path)
+        {
+            ex = new Application();
+            ex.Workbooks.Open(path);
+            ex.Workbooks[1].Activate();
+            ((Worksheet)this.ex.ActiveWorkbook.Sheets[1]).Select();
+        }
+
+        public void AddSheet(string name)
+        {
+            ex.Sheets.Add(name);
+        }
+
+        public Worksheet GetWorksheet(string name) => ex.Sheets[name];
+
+        public void RemoveSheet(string name) => ex.Sheets[name].Delete();
+
+        public void SetColumnName(string name, int col)
+        {
+            ex.Sheets[1].Cells[1, col].Value = name;
+        }
+
+        public void SetRowName(string name, int row)
+        {
+            ex.Sheets[1].Cells[row, 2].Value = name;
+        }
+
+        //public void AutoF()
+        //{
+        //    Worksheet sheet = ex.Worksheets.Item[1];
+        //    ((Worksheet)sheet.Columns[1]).
+        //}
+
+        public void Show()
+        {
+            ex.Visible = true;
+        }
+
+        public void SaveAs(string path)
+        {
+            ex.Workbooks[1].SaveAs2(path);
+        }
+
+        public void FillRow(string data, int row)
+        {
+            string[] subd = data.Split(' ');
+            for (int i = 0; i < subd.Length; i++)
+            {
+                ex.Cells[row, i + 3].Value = subd[i];
+            }
+        }
+    }
+}
---
+++ Tableproj/Program.cs
@@ -0,0 +1,20 @@
+﻿using Tableproj.Model;
+
+ExcelClass excel = new ExcelClass("C:\\Users\\trale\\OneDrive\\Desktop\\doc.xlsx");
+//string name = Console.ReadLine();
+
+//excel.SetRowName(name, 2);
+//excel.SetRowName(name, 3);
+//excel.SetRowName(name, 4);
+//excel.SetRowName(name, 5);
+//excel.SetRowName(name, 6);
+//excel.SetColumnName(name, 1);
+//excel.SetColumnName(name, 2);
+//excel.SetColumnName(name, 3);
+//excel.SetColumnName(name, 4);
+//excel.SetColumnName(name, 5);
+//excel.SetColumnName(name, 6);
+excel.FillRow("5 42 255 14.1 56 78 846 22.1 74", 4);
+excel.Show();
+excel.SaveAs("C:\\Users\\trale\\OneDrive\\Desktop\\doc2.xlsx");
+//excel.SaveFile("doc.xlsx");
